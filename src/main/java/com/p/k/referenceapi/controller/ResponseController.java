7bfc9bde4e542f16a22c65f99413124092f09314
xpavkov <pavlo.kovgan@ericsson.com>
2023-04-21 18:26:22 +0300

    Add JSON response

--- src/main/java/com/p/k/referenceapi/controller/ResponseController.java
+++ src/main/java/com/p/k/referenceapi/controller/ResponseController.java
@@ -1,12 +1,18 @@
 package com.p.k.referenceapi.controller;
 
+import com.p.k.referenceapi.entity.Mapping;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ResponseController {
     @GetMapping(value = "/")
-    public String getMainPage() {
-        return "Hello";
+    public ResponseEntity<Mapping> getResponse() {
+        Mapping mapping = new Mapping();
+        mapping.setAc("111111");
+        mapping.setABCCopy("ABC");
+        mapping.setEntity("A");
+        return ResponseEntity.ok(mapping);
     }
 }
---
+++ src/main/java/com/p/k/referenceapi/entity/Mapping.java
@@ -0,0 +1,10 @@
+package com.p.k.referenceapi.entity;
+
+import lombok.Data;
+
+@Data
+public class Mapping {
+    private String ac;
+    private String ABCCopy;
+    private String Entity;
+}
